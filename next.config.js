806dcb5bec5a1ad43ffdbceb32c7b381276d8ff5
sofiagalkina <gal18024@byui.edu>
2024-12-15 22:04:44 -0700

    file fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     eslint: {
       ignoreDuringBuilds: true,
     },
   };
+  
+  module.exports = nextConfig;
   
